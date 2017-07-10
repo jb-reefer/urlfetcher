a861e906c7c79b047eeacb785c46bc53ea8fe038
jb-reefer <james.reefer@gmail.com>
2017-07-10 12:05:24 -0400

    Works with results!

--- urlfetcher.go
+++ urlfetcher.go
@@ -16,27 +16,27 @@ import (
 )
 
 var (
-	term    = flag.String("query", "test", "Search word/phrase in webpages")
+	term        = flag.String("query", "test", "Search word/phrase in webpages")
+	urlFilename = flag.String("file", "urls.txt", "CSV formatted file containg urls")
+	threads     = flag.Int("threads", 20, "Number of threads to use")
+
 	matcher = search.New(language.AmericanEnglish, search.IgnoreCase)
 	wait    = sync.WaitGroup{}
 )
 
-// FindResult Results object from worker threads
+// FindResult for a given term in webpage at URL
 type FindResult struct {
 	FoundMatch bool
 	URL        string
 }
 
 func (result FindResult) String() string {
-	return fmt.Sprintf("Found: %t\tURL: %v\n", result.FoundMatch, result.URL)
+	return fmt.Sprintf("Found: %t\tURL: %v", result.FoundMatch, result.URL)
 }
 
 func main() {
 
 	// Load and parse file
-	var urlFilename = flag.String("file", "urls.txt", "CSV formatted file containg urls")
-	var threads = flag.Int("threads", 20, "Number of threads to use")
-
 	flag.Parse()
 
 	file, err := os.Open(*urlFilename)
@@ -58,7 +58,7 @@ func main() {
 
 	// Create workers and process workload
 	urlQueue := make(chan string)
-	results := make(chan FindResult)
+	results := make(chan FindResult, len(fileData))
 
 	// Spawn workers
 	for i := 0; i < *threads; i++ {
@@ -73,24 +73,38 @@ func main() {
 	// Await and read out results
 	wait.Wait()
 	close(urlQueue)
+	close(results)
 
 	var buffer bytes.Buffer
 
+	fmt.Println("Reading results...")
+
+	i := 1
+
 	for result := range results {
+		fmt.Println(result)
+		fmt.Println(i)
+		i++
 		buffer.WriteString(result.String())
 		buffer.WriteString("\n")
 
 	}
 
-	close(results)
+	fmt.Println("Read all results.")
 
-	ioutil.WriteFile("results.txt", buffer.Bytes(), 0644)
+	fileErr := ioutil.WriteFile("results.txt", buffer.Bytes(), 0644)
+
+	if fileErr != nil {
+		fmt.Println("Could not write out results.txt.")
+		fmt.Println(fileErr)
+		return
+	}
 
 	fmt.Println("All URLs parsed, exiting.")
 
 }
 
-func searchWorker(input chan string, output chan FindResult) {
+func searchWorker(input chan string, output chan<- FindResult) {
 
 	// Grab urls off of the queue
 	for url := range input {
@@ -119,18 +133,20 @@ func searchURLForTerm(url string) FindResult {
 		response, wwwError = http.Get(address)
 
 		if wwwError != nil {
-			fmt.Printf("Could not resolve %v:\t%v\n", url, wwwError)
-			return FindResult{false, address}
+			//fmt.Printf("Could not resolve %v:\t%v\n", url, wwwError)
+			return FindResult{false, url}
 		}
 	}
 
 	defer response.Body.Close()
 
-	// TODO: this error handling
-	body, _ := ioutil.ReadAll(response.Body)
-	start, _ := matcher.Index(body, []byte(*term))
+	body, readErr := ioutil.ReadAll(response.Body)
 
-	result := FindResult{start != -1, address}
-	fmt.Print(result)
-	return result
+	if readErr != nil {
+		fmt.Printf("Could not read body for page %v, got error:%v\n", url, readErr)
+		return FindResult{false, url}
+	}
+
+	start, _ := matcher.Index(body, []byte(*term))
+	return FindResult{start != -1, address}
 }
